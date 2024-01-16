9608b6f179758be3a0f2abb0c6113de493d440f7
BhavikBhatt <bhavikyb@gmail.com>
2024-01-16 15:56:47 -0500

    cleaned up

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -4,46 +4,87 @@ import pymongo
 import boto3
 from langchain_community.embeddings.bedrock import BedrockEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+import PyPDF2
 
 
-
-#url = 'mongodb://localhost:27017'
 url = 'mongodb+srv://<username>:<password>@cluster0.bjpsr.mongodb.net/?retryWrites=true&w=majority'
-
 mongoClient = pymongo.MongoClient(url)
 db = mongoClient['hackathon']
-collection = db['1000_chunk']
-
+collection = db['hackathon_rag']
 
 bedrock_client = boto3.client(service_name="bedrock-runtime", region_name="us-east-1")
-bedrock_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1",
-                                       client=bedrock_client)
 
-page = requests.get('https://www.mongodb.com/docs/atlas/atlas-search/create-index/')
-tree = html.fromstring(page.content)
+pdf_path = 'genai-principles.pdf'
 
-content = tree.xpath('//p[@class="leafygreen-ui-kkpb6g"]/text()')
+def extract_text_from_pdf(pdf_path):
+    text = ""
 
-separator = ' '
-text = separator.join(content)
+    # Open the PDF file
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
 
-text_splitter = RecursiveCharacterTextSplitter(
-    # Set a really small chunk size, just to show.
-    chunk_size=1000,
-    chunk_overlap=100,
-)
-chunks = text_splitter.split_text(text)
+        # Iterate over each page and extract text
+        for page_num in range(len(reader.pages)):
+            page = reader.pages[page_num]
+            text += page.extract_text()
 
-for chunk in chunks:
-    embedding = bedrock_embeddings.embed_query(chunk)
-    payload = {
-        "text": chunk,
-        "embeddings": embedding
-    }
-    collection.insert_one(payload)
+    text = text.replace('\n', ' ').replace('\r', '')
+    return text
 
 
+def process_chunks(chunk_size, chunk_overlap, text, model_id="amazon.titan-embed-text-v1"):
+
+    collection.delete_many({})
+
+    bedrock_embeddings = BedrockEmbeddings(model_id=model_id, client=bedrock_client)
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+    )
+    chunks = text_splitter.split_text(text)
+
+    for chunk in chunks:
+        embedding = bedrock_embeddings.embed_query(chunk)
+        payload = {
+            "text": chunk,
+            "embeddings": embedding
+        }
+        collection.insert_one(payload)
 
 
+def ask_question(question, model_id):
 
+    bedrock_embeddings = BedrockEmbeddings(model_id=model_id, client=bedrock_client)
 
+    embedding = bedrock_embeddings.embed_query(question)
+
+    context = collection.aggregate([{
+        "$vectorSearch": {
+            "queryVector": embedding,
+            "path": "embeddings",
+            "numCandidates": 10,
+            "index": "rag",
+            "limit": 1
+            }
+        },
+        {
+            "$project": {
+                "embeddings": 0
+            }
+        }
+    ])
+
+    print(list(context))
+    for l in list(context):
+        print(l['text'])
+
+def main():
+    
+    extracted_text = extract_text_from_pdf(pdf_path)
+
+    process_chunks(500, 50, extracted_text, model_id="amazon.titan-embed-text-v1")
+
+main()
+    
+ask_question("What are large language models used for?", model_id="amazon.titan-embed-text-v1")
