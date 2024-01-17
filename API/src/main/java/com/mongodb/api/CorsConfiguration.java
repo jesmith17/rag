76f5a671c126d2c39ab241793abc945f761e3acb
Josh Smith <josh.smith@mongodb.com>
2024-01-17 10:07:57 -0600

    Fixed CORS

--- API/src/main/java/com/mongodb/api/CorsConfiguration.java
+++ API/src/main/java/com/mongodb/api/CorsConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
-                .allowedOrigins("**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
     }
 }
